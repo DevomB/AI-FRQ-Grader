660a5872c87f3aed1efe3a40fea46fa15bd193f6
Praneet <praneetdumpa@gmail.com>
2025-01-09 21:12:11 -0800

    add textbox

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-// filepath: /Users/dev/Documents/GitHub/AI-FRQ-Grader/pages/index.js
 import React from 'react';
 import styles from '../app/styles/Home.module.css';
 
@@ -6,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Upload Your Files</h1>
+      <input type="text" placeholder="Enter some text" className={styles.textBox} />
       <div className={styles.uploadContainer}>
         <div className={styles.uploadBox}>
           <input type="file" id="fileUpload1" className={styles.fileInput} />
@@ -13,11 +13,11 @@ export default function Home() {
         </div>
         <div className={styles.uploadBox}>
           <input type="file" id="fileUpload2" className={styles.fileInput} />
-          <label htmlFor="fileUpload2" className={styles.uploadLabel}>Upload File</label>
+          <label htmlFor="fileUpload2" className={styles.uploadLabel}>Upload Image</label>
         </div>
         <div className={styles.uploadBox}>
           <input type="file" id="fileUpload3" className={styles.fileInput} />
-          <label htmlFor="fileUpload3" className={styles.uploadLabel}>Upload File</label>
+          <label htmlFor="fileUpload3" className={styles.uploadLabel}>Upload Video</label>
         </div>
       </div>
     </div>
