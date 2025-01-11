f3fdbf682277d1e294151418fd4e9b653e3b8902
SireSoul <satyask.lakkaraju@gmail.com>
2025-01-10 18:27:33 -0800

    UI Change #2

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
           {/* File Upload 1 */}
           <div>
             <div
-              className="flex flex-col justify-center items-center border-2 border-dashed border-blue-900 rounded-lg p-6 bg-gradient-to-b from-green-100 to-blue-100 hover:bg-blue-50 transition shadow-sm relative"
+              className="flex flex-col justify-center items-center border-2 border-dashed border-blue-900 rounded-lg p-6 bg-blue-100 hover:bg-blue-50 transition shadow-sm relative"
             >
               <input
                 type="file"
@@ -65,7 +65,7 @@ export default function Home() {
           {/* File Upload 2 */}
           <div>
             <div
-              className="flex flex-col justify-center items-center border-2 border-dashed border-yellow-700 rounded-lg p-6 bg-gray-50 hover:bg-orange-50 transition shadow-sm relative"
+              className="flex flex-col justify-center items-center border-2 border-dashed border-yellow-700 rounded-lg p-6 bg-yellow-50 hover:bg-white transition shadow-sm relative"
             >
               <input
                 type="file"
@@ -74,7 +74,7 @@ export default function Home() {
               />
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12 text-orange-600 hover:scale-110 transition"
+                className="h-12 w-12 text-yellow-600 hover:scale-110 transition"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -86,7 +86,7 @@ export default function Home() {
                   d="M3 10l9-9m0 0l9 9m-9-9v16"
                 />
               </svg>
-              <p className="mt-2 text-orange-700 font-bold text-lg">Upload Your Response</p>
+              <p className="mt-2 text-yellow-700 font-bold text-lg">Upload Your Response</p>
               <p className="mt-1 text-gray-500 text-sm">Supports PDF, DOCX, TXT</p>
             </div>
           </div>
